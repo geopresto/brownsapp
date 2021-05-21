008c88a719e643fd8ad00a5422c7291483f7a85e
Geovante Preston <geovante@rvshare.com>
2021-05-21 13:00:39 -0400

    ?

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 config.assets.initialize_on_precompile = false
 
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
