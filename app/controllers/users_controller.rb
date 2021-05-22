e3d2cfe03977d3c3a697f93549d88dceb3c1dd87
Geovante Preston <geovante@rvshare.com>
2021-05-22 16:43:30 -0400

    prior to revert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,6 @@ class UsersController < ApplicationController
   end
 
   def show
-  end
-  
-  private
-  
-  def set_user 
     @user = User.find(params[:id])
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_attached_file :avatar
     has_many_attached :images
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+    
 
 end
