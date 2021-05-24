cf38c062d37a9616a2c09925a59d69d052308e38
Geovante Preston <geovante@rvshare.com>
2021-05-23 21:24:40 -0400

    More Styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ skip_before_action :authorized, only: [:new, :create, :welcome]
     def create
       @user = User.find_by(username: params[:username])
       if @user && @user.authenticate(params[:password])
-        sessions[:user_id] = @user.id
+        
         redirect_to '/welcome'
       else
         redirect_to '/login'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
+  get 'users', to: 'sessions#create'
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
