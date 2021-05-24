a3d8aa45f974359d6b0744053b3e4c0257444ef7
Geovante Preston <geovante@rvshare.com>
2021-05-24 16:46:35 -0400

    removed bad gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_one_attached :avatar
     has_many_attached :images
-    has_many_attached :posts
     
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'photo/index'
   get 'photo/store'
   resources :users, only: [:new, :create, :show]
-  resources :posts
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
